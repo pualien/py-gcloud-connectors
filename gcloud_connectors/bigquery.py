27c891e9fe496206b9626f48ebcb304c836f368d
msenardi <matteo.s@docsity.com>
2020-05-08 13:12:47 +0200

    added bigquery_storage_v1beta1

--- gcloud_connectors/bigquery.py
+++ gcloud_connectors/bigquery.py
@@ -2,6 +2,7 @@
 import os
 
 import pandas_gbq
+from google.cloud.bigquery_storage_v1beta1 import BigQueryStorageClient
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
@@ -28,13 +29,22 @@ class BigQueryConnector:
 
         self.service = bigquery.Client(project=self.project_id, credentials=self.creds)
 
-    def pd_execute(self, query, progress_bar_type=None):
+    def pd_execute(self, query, progress_bar_type=None, bqstorage_enabled=False):
         """
 
         :param query:
         :param progress_bar_type:
         :return:
         """
-        return pandas_gbq.read_gbq(query, project_id=self.project_id, credentials=self.creds, progress_bar_type=progress_bar_type)
+
+        # Use a BigQuery Storage API client to download results more quickly.
+        if bqstorage_enabled is True:
+            bqstorage_client = BigQueryStorageClient(
+                credentials=self.creds
+            )
+            return self.service.query(query).to_arrow(bqstorage_client=bqstorage_client, progress_bar_type=progress_bar_type).to_pandas()
+        else:
+            return self.service.query(query).to_dataframe(progress_bar_type=progress_bar_type)
+        # return pandas_gbq.read_gbq(query, project_id=self.project_id, credentials=self.creds, progress_bar_type=progress_bar_type, use_bqstorage_api=True)
 
 
