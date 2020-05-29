b5ae63bfe111ed3a2737597528534448c3482fc7
msenardi <matteo.s@docsity.com>
2020-05-29 12:34:06 +0200

    added get_pandas_dtypes for bigquery column casting

--- gcloud_connectors/bigquery.py
+++ gcloud_connectors/bigquery.py
@@ -26,6 +26,20 @@ class BigQueryConnector:
 
         self.service = bigquery.Client(project=self.project_id, credentials=self.creds)
 
+    def get_pandas_dtypes(self, dataset, table):
+        table_ref = '{}.{}'.format(dataset, table)
+        table = self.service.get_table(table_ref)
+        schema_dtypes = {}
+        for schema in table.schema:
+            if schema.field_type in ['STRING', 'DATE']:
+                type = 'category'
+            elif schema.field_type in ['FLOAT', 'BOOLEAN', 'INTEGER']:
+                type = schema.field_type.lower()
+            else:
+                raise AttributeError('Unknown type {} for {}'.format(schema.field_type, schema.name))
+            schema_dtypes[schema.name] = type
+        return schema_dtypes
+
     def pd_execute(self, query, progress_bar_type=None, bqstorage_enabled=False):
         """
 
