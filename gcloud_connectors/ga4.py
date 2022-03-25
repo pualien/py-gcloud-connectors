98ce07b983446df22f99e45c3aa794054c6edb0b
Matteo Senardi <pualien@gmail.com>
2022-03-25 10:19:21 +0100

    added GAnalytics4Connector

--- gcloud_connectors/ga4.py
+++ gcloud_connectors/ga4.py
@@ -1,55 +1,186 @@
-import os
-# Set environment variables
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "service_account_file_path"
-
+import google.auth
+import pandas as pd
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import DateRange
 from google.analytics.data_v1beta.types import Dimension
+from google.analytics.data_v1beta.types import Filter
+from google.analytics.data_v1beta.types import FilterExpression
+from google.analytics.data_v1beta.types import FilterExpressionList
 from google.analytics.data_v1beta.types import Metric
 from google.analytics.data_v1beta.types import RunReportRequest
-import pandas as pd
-
-import pandas as pd
-from datetime import datetime, timedelta
-from oauth2client.service_account import ServiceAccountCredentials
-from retry import retry
+from google.oauth2 import service_account
 
 from gcloud_connectors.logger import EmptyLogger
 
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 
 
-class GAError(Exception):
-    """Base class for GA exceptions"""
+class GAnalytics4Connector:
+    def __init__(self, confs_path=None, auth_type='service_account', json_keyfile_dict=None, logger=None):
+        self.confs_path = confs_path
+        self.json_keyfile_dict = json_keyfile_dict
+        self.auth_type = auth_type
+        self.logger = logger if logger is not None else EmptyLogger()
 
+        if self.confs_path is not None or self.json_keyfile_dict is not None:
+            if self.confs_path is not None:
+                self.creds = service_account.Credentials.from_service_account_file(
+                    self.confs_path,
+                )
+            elif self.json_keyfile_dict is not None:
+                self.creds = service_account.Credentials.from_service_account_info(
+                    self.json_keyfile_dict
+                )
 
-class GAPermissionError(GAError):
-    """Base class for GA permission exceptions"""
-    pass
+        else:
+            self.creds, project = google.auth.default()
 
-def sample_run_report(property_id, export_path):
-    """Runs a simple report on a Google Analytics 4 property."""
+        self.logger = logger if logger is not None else EmptyLogger()
+        self.service = BetaAnalyticsDataClient(credentials=self.creds)
 
-    # Using a default constructor instructs the client to use the credentials
-    # specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
-    client = BetaAnalyticsDataClient()
+    @staticmethod
+    def get_base_report_params(property_id, metrics_dict_values, dimensions_dict_values, date_range, limit, offset,
+                               keep_empty_rows, return_property_quota):
+        return {
+            "property": "properties/{property_id}".format(property_id=property_id),
+            "dimensions": dimensions_dict_values,
+            "metrics": metrics_dict_values,
+            "date_ranges": date_range,
+            "limit": limit,
+            "offset": offset,
+            "keep_empty_rows": keep_empty_rows,
+            "return_property_quota": return_property_quota
+        }
 
-    request = RunReportRequest(
-        property=f"properties/{property_id}",
-        dimensions=[Dimension(name="city")],
-        metrics=[Metric(name="activeUsers")],
-        date_ranges=[DateRange(start_date="2020-03-31", end_date="today")],
-    )
-    response = client.run_report(request)
+    @staticmethod
+    def get_filter_type(filter_type_string):
+        filter_type = Filter.StringFilter  # default
+        if filter_type_string == "string_filter":
+            filter_type = Filter.StringFilter
+        elif filter_type_string == "in_list_filter":
+            filter_type = Filter.InListFilter
+        elif filter_type_string == "numeric_filter":
+            filter_type = Filter.NumericFilter
+        elif filter_type_string == "between_filter":
+            filter_type = Filter.BetweenFilter
+        return filter_type
 
-    output = []
-    print("Report result:")
-    for row in response.rows:
-        output.append({"City": row.dimension_values[0].value, "Active Users": row.metric_values[0].value})
-    df = pd.DataFrame(output)
-    df.to_csv(export_path)
+    def get_filters(self, filters):
+        """
+        filters = [{"field_name": "browser", "type": "string_filter", "value": "chrome", "match_type": 1,
+        "is_not": True}]
+        """
+        filter_dict_values = []
+        for filter_object in filters:
+            filter_type = self.get_filter_type(filter_object["type"])
+            if filter_object.get("match_type"):
+                filter_applied = filter_type(
+                    {"value": filter_object["value"], "match_type": filter_object["match_type"]}
+                )
+            else:
+                filter_applied = filter_type({"value": filter_object["value"]})
 
+            # TODO: wait to be fixed combining not_expressions inside FilterExpressionList. In march 2022 leads to
+            #  TypeError: Parameter to MergeFrom() must be instance of same class: expected FilterExpression
+            #  got Filter. for field FilterExpression.not_expression
+            if filter_object.get("is_not"):
+                filter_dict_values.append(
+                    FilterExpression({
+                        "not_expression": Filter({
+                            "field_name": filter_object["field_name"],
+                            filter_object["type"]: filter_applied
 
-sample_run_report(PROPERTY_ID, "export.csv")
-df = pd.read_csv("export.csv")
-df.head(5)
+                        }
+                        )
+                    })
+                )
+            else:
+                filter_dict_values.append(
+                    FilterExpression({
+                        "filter": Filter({
+                            "field_name": filter_object["field_name"],
+                            filter_object["type"]: filter_applied
+
+                        }
+                        )
+                    })
+                )
+        return filter_dict_values
+
+    @staticmethod
+    def get_df_from_response(response, dimensions, metrics):
+        output = []
+        for row in response.rows:
+            current_row = {}
+            for dim in dimensions:
+                current_row[dim] = row.dimension_values[dimensions.index(dim)].value
+            for metr in metrics:
+                current_row[metr] = row.metric_values[metrics.index(metr)].value
+            output.append(current_row)
+        return pd.DataFrame(output)
+
+    def pd_get_report(self, property_id, start_date, end_date, metrics, dimensions, dimension_filters=None,
+                      metrics_filters=None, offset=0, limit=100000,
+                      keep_empty_rows=False, return_property_quota=False, downloaded_totals=0, cast_date_column=None):
+        """Runs a complex report on a Google Analytics 4 property."""
+        dimensions_dict_values = [Dimension({"name": x}) for x in dimensions]
+        metrics_dict_values = [Metric({"name": x}) for x in metrics]
+        date_range = [DateRange({"start_date": start_date, "end_date": end_date})]
+
+        if dimension_filters is None:
+            dimension_filters = []
+        if metrics_filters is None:
+            metrics_filters = []
+
+        dimension_filter_dict_values = self.get_filters(dimension_filters)
+        metric_filter_dict_values = self.get_filters(metrics_filters)
+
+        report_params = self.get_base_report_params(property_id=property_id, metrics_dict_values=metrics_dict_values,
+                                                    dimensions_dict_values=dimensions_dict_values,
+                                                    date_range=date_range, limit=limit, offset=offset,
+                                                    keep_empty_rows=keep_empty_rows,
+                                                    return_property_quota=return_property_quota)
+        if len(dimension_filter_dict_values):
+            report_params["dimension_filter"] = FilterExpression({
+                "and_group": FilterExpressionList(
+                    {
+                        "expressions": dimension_filter_dict_values
+
+                    }
+                )
+            })
+        if len(metric_filter_dict_values):
+            report_params["metric_filter"] = FilterExpression({
+                "and_group": FilterExpressionList(
+                    {
+                        "expressions": metric_filter_dict_values
+                    }
+                )
+            })
+
+        request = RunReportRequest(report_params)
+
+        response = self.service.run_report(request)
+
+        df = self.get_df_from_response(response, dimensions, metrics)
+
+        # if missing pages for all rows in response.row_count
+        if len(response.rows) < response.row_count and downloaded_totals < response.row_count:
+            downloaded_totals = len(response.rows) + downloaded_totals
+            print(
+                "Recursive iteration page {current_page}/{pages} for records {records}/{totals}".format(
+                    records=offset + limit if offset + limit < response.row_count else response.row_count,
+                    totals=response.row_count,
+                    current_page=round(downloaded_totals / limit),
+                    pages=round(response.row_count / limit)
+                ))
+            df = df.append(
+                self.pd_get_report(property_id=property_id, start_date=start_date, end_date=end_date, metrics=metrics,
+                                   dimensions=dimensions, dimension_filters=dimension_filters,
+                                   metrics_filters=metrics_filters, offset=offset + limit, limit=limit,
+                                   keep_empty_rows=keep_empty_rows, return_property_quota=return_property_quota,
+                                   downloaded_totals=downloaded_totals))
+        if cast_date_column:
+            if 'date' in df.columns:
+                df['date'] = pd.to_datetime(df['date'], format="%Y%m%d").dt.date
+        return df
